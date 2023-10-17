07351065bdc8d04b12e6b01bb8441eb23e5aad6e
Brian Fulton-Howard <brian.fulton-howard@mssm.edu>
2023-10-17 15:16:31 -0400

    Update for large files + shapeit5 + admix ref

--- workflow/scripts/rule_process_smap_1kg_hgdp.R
+++ workflow/scripts/rule_process_smap_1kg_hgdp.R
@@ -3,40 +3,48 @@ library(tidyr)
 suppressPackageStartupMessages(library(dplyr))
 library(purrr)
 
-input_metadata <- "Downloads/gnomad.genomes.v3.1.2.hgdp_1kg_subset_sample_meta.tsv.bgz"
+input_metadata <-
+  "Downloads/gnomad.genomes.v3.1.2.hgdp_1kg_subset_sample_meta.tsv.bgz"
 output_table <- "reference_proc/hgdp_1kg.popdata.tsv.gz"
 output_smap <- "reference_proc/hgdp_1kg.smap"
 
 input_metadata <- snakemake@input[["metadata"]]
-input_samplist <- snakemake@input[["samplist"]]
 output_table <- snakemake@output[["table"]]
 output_smap <- snakemake@output[["smap"]]
 
-
 parse_gnomad <- Vectorize(function(x) {
+    # if the input is NA, return NA
     if (is.na(x)) {
         return(NA)
+    # otherwise, parse the JSON
     } else {
         return(jsonlite::fromJSON(x))
     }
-}, SIMPLIFY = F, USE.NAMES = F)
-
-samplist <- input_samplist |>
-  read_lines()
+}, SIMPLIFY = FALSE, USE.NAMES = FALSE)
 
 poptab <- input_metadata |>
   read_tsv() |>
-  select(s, gnomad_population_inference, high_quality) |>
-  mutate(popinf = map_vec(gnomad_population_inference, parse_gnomad)) |>
+  select(s, gnomad_population_inference, hgdp_tgp_meta, high_quality) |>
+  mutate(popinf = map_vec(gnomad_population_inference, parse_gnomad),
+         metadata =  map_vec(hgdp_tgp_meta, parse_gnomad)) |>
   filter(!is.na(popinf) & !is.na(high_quality), high_quality) |>
-  select(-high_quality, -gnomad_population_inference) |>
-  unnest_wider(popinf) |>
-  rename(pc = pca_scores) |>
+  select(-high_quality, -gnomad_population_inference, -hgdp_tgp_meta) |>
+  unnest_wider(all_of(c("popinf", "metadata"))) |>
+  mutate(pop_proc = case_when(
+           pop %in% c("fin", "nfe") ~ "EUR",
+           genetic_region == "OCE" ~ "OCE",
+           TRUE ~ toupper(pop)),
+         spop_checked = case_when(
+           genetic_region == "CSA" & pop_proc == "SAS" ~ "SAS",
+           genetic_region == pop_proc ~ pop_proc,
+           TRUE ~ NA_character_)) |>
+  select(-hgdp_technical_meta, -global_pca_scores, -subcontinental_pca) |>
+  rename(pc = pca_scores, pop_orig = pop, spop = pop_proc, pop = population,
+         IID = s) |>
   unnest_wider(pc, names_sep = "") |>
-  filter(s %in% samplist) |>
+  select(IID, pop, spop, everything()) |>
   write_tsv(output_table)
 
 poptab |>
-  select("#Sample" = "s", "Panel" = "pop") |>
+  select("#Sample" = "IID", "Panel" = "spop") |>
   write_tsv(output_smap)
-
